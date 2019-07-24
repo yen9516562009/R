3daa455480c744b6b55327e3eaf12307743e34d3
yen9516562009 <yen9516562009@gmail.com>
2019-07-24 16:48:52 -0700

    Update Merge CSV files.R

--- Merge CSV files/Merge CSV files.R
+++ Merge CSV files/Merge CSV files.R
@@ -18,12 +18,13 @@ for (dir in PT){
   myfiles = list.files(path = dir, pattern="*.csv", full.names = TRUE)
   dat_csv = ldply(myfiles, read_csv)
   
+  #output csv
   file = paste("",dir, sep="",".csv")
   write.csv(dat_csv, file, row.names=FALSE)
 
-  # calculate total # of rows in each merged property table (exclude header)
+  #calculate total # of rows in each merged property table (exclude header)
   NR[[dir]] <- nrow(dat_csv)-1
   
-  # reset dat_csv by removing it
+  #reset dat_csv by removing it
   rm(dat_csv)
 }
